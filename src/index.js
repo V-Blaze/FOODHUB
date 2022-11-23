2d25222f2e2bf106a9159e021eaf6bb02960054b
Usama Masood <usamamaliq@gmail.com>
2022-11-24 02:06:49 +0500

    Add functionality to show likes from API

--- src/index.js
+++ src/index.js
@@ -2,5 +2,3 @@ import './style.css';
 import {showMeals} from './modules/displayData.js';
 
 showMeals();
-
-// console.log(process.env.APP_ID);
--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -1,14 +1,5 @@
-import { addPopUpEvent } from './popup-details.js';
-
-
-
-let newMealsArray=[];
-let likesArray=[];
 
 const display = (meal) => {
-  // const homePageContainer = document.querySelector('.homePageContainer');
-  // homePageContainer.innerHTML = '';
-  // newMealsArray.map((item) => {
     const mealContainer= document.createElement('div');
     mealContainer.className = "mainItemContainer"
     mealContainer.innerHTML = `
@@ -18,7 +9,7 @@ const display = (meal) => {
           <h2 class="itemName">${meal.strMeal}</h2>
             <div class="likesContainer">
              <i class="fa-solid fa-heart heart"></i>
-             <h4 class="likeCounter">5 likes</h4>
+             <h4 class="likeCounter">${meal.likes} likes</h4>
             </div>
         </div>
         <div class="commentButtonContainer">
@@ -26,54 +17,28 @@ const display = (meal) => {
       </div>
       </div>`;
     return mealContainer;
-  // });
 }
-
 
 const displayData = async () => {
   const baseUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=American';
   const response= await fetch(baseUrl) 
   const data = await response.json();
-  // console.log(data.meals) 
   return data.meals;
-  
-    // .then((res) => res.json())
-    // .then((data) => {
-    //   return data.meals;
-      // let mealsArray = data.meals;
-      // mealsArray.forEach(element => {
-      //   newMealsArray.push(element);
-      // });
-      
-      // fetchLikes();
-      // display();
-      // addPopUpEvent();
-    // });
 };
 
 const fetchLikes = async () => {
   const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
-//  await fetch (`${baseUrl}${process.env.APP_ID}/likes`)
-//  await fetch (`${baseUrl}DyyFn7Hy0lkWRXlkcSbQ/likes`)
-  const response= await fetch(`${baseUrl}DyyFn7Hy0lkWRXlkcSbQ/likes`)
+  const response= await fetch(`${baseUrl}${process.env.APP_ID}/likes`)
   const data = await response.json();
   return data;
-    // .then((res) => res.json())
-    // .then((data) => {
-    //   return data;
-    //   let likes = data;
-      
-    //   likes.forEach(element => {
-    //     likesArray.push(element);
-    //   });
-    // });
 };
+
 const updateLikes = (newMealsArray) => {
   let updatedMealsArray = [];
   newMealsArray.forEach(meal => {
-    let existing = updatedMealsArray.filter((item, index) => {
-      return item.idMeal == item.item_id;
-    }) 
+    let existing = updatedMealsArray.filter(item => {
+      return item.idMeal == meal.item_id;
+    })
     if (existing.length)
     {
       let existingIndex = updatedMealsArray.indexOf(existing[0]);
@@ -85,6 +50,7 @@ const updateLikes = (newMealsArray) => {
   })
   return updatedMealsArray;
 }
+
 export const  showMeals = async () => {
   const homePageContainer = document.querySelector('.homePageContainer');
   homePageContainer.innerHTML = '';
@@ -93,16 +59,9 @@ export const  showMeals = async () => {
     meal.likes = 0;
   })  
   let likesArray = await fetchLikes();
-
-  // console.log(fe)
   let newMealsArray = mealsArray.concat(likesArray);
-  // console.log(newMealsArray);
   let updatedMealsArray = updateLikes(newMealsArray);
-  console.log(updatedMealsArray);
-  mealsArray.forEach(meal => {
+  updatedMealsArray.forEach(meal => {
     homePageContainer.append(display(meal));
   });
 }
-// console.log(likesArray);
-// console.log(newMealsArray);
-// export default displayData;
