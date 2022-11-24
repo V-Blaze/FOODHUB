1cf09c0d428c16c3fd6bd91986086a2219fee3b2
Usama Masood <usamamaliq@gmail.com>
2022-11-25 00:06:33 +0500

    Add test for likes counter

--- src/modules/itemCounter.js
+++ src/modules/itemCounter.js
@@ -1,8 +1,7 @@
 const itemCounter = () => {
   const countAll = document.querySelectorAll('.mainItemContainer').length;
   const itemCounterContainer = document.querySelector('.itemCounterContainer');
-  // itemCounterContainer.textContent = `Meals(${countAll})`;
-  // itemCounterContainer.innerHTML = `Meals(${countAll})`;
+  itemCounterContainer.textContent = `Meals(${countAll})`;
   return `Meals(${countAll})`;
 };
 export default itemCounter;
--- src/test/itemCounter.test.js
+++ src/test/itemCounter.test.js
@@ -1,17 +1,10 @@
 /** @jest-environment jsdom */
+import { constant } from 'lodash';
 import itemCounter from '../modules/itemCounter.js';
 
 describe('Count No of comments on the DOM', () => {
-beforeEach(() => {
-    document.body.innetHTML = `
-<nav class="menu">
-    <ul class="menu-list">
-        <li class="list-item menuList"><a href="#">Home</a></li>
-        <li class="list-item menuList"><a class="itemCounterContainer" href="#"></a></li>
-    </ul>
-</nav>
-`;
-})
+const item = document.querySelector('.menu-list');
+console.log(item);
     const meals = [
     {
       strMeal: 'first meal Name',
@@ -53,44 +46,39 @@ beforeEach(() => {
   ];
   
   
-// document.body.innetHTML = `
-// <nav class="menu">
-//     <ul class="menu-list">
-//         <li class="list-item menuList"><a href="#">Home</a></li>
-//         <li class="list-item menuList"><a class="itemCounterContainer" href="#"></a></li>
-//     </ul>
-// </nav>
-// `;
+document.body.innerHTML = `
+<nav class="menu">
+    <ul class="menu-list">
+        <li class="list-item menuList"><a href="#">Home</a></li>
+        <li class="list-item menuList"><a class="itemCounterContainer" href="#"></a></li>
+    </ul>
+</nav>
+`;
 
-//   meals.map((meal) => {
-//   document.body.innerHTML += `<div class="mainItemContainer">
-//     <div class="itemContainer">
-//         <a class="itemImageContainer" href=""><img class="itemImage" src="${meal.strMealThumb}" alt="meal picture"></a>
-//         <div id="hello" class="itemNameContainer">
-//           <h2 class="itemName">${meal.strMeal}</h2>
-//             <div  class="likesContainer">
-//              <i class="fa-solid fa-heart heart like${meal.idMeal}"></i>
-//              <h4 class="likeCounter">${meal.likes} likes</h4>
-//             </div>
-//         </div>
-//         <div class="commentButtonContainer">
-//         <button id="${meal.idMeal}" type="button" class="commentsButton">Comments</button>
-//       </div>
-//       </div>
-//       </div>`;
-//     return 0;
+  meals.map((meal) => {
+  document.body.innerHTML += `<div class="mainItemContainer">
+    <div class="itemContainer">
+        <a class="itemImageContainer" href=""><img class="itemImage" src="${meal.strMealThumb}" alt="meal picture"></a>
+        <div id="hello" class="itemNameContainer">
+          <h2 class="itemName">${meal.strMeal}</h2>
+            <div  class="likesContainer">
+             <i class="fa-solid fa-heart heart like${meal.idMeal}"></i>
+             <h4 class="likeCounter">${meal.likes} likes</h4>
+            </div>
+        </div>
+        <div class="commentButtonContainer">
+        <button id="${meal.idMeal}" type="button" class="commentsButton">Comments</button>
+      </div>
+      </div>
+      </div>`;
+    return 0;
     
-//   });
+  });
   
-  
-
   test('to check if number count is 6', () => {
-    
     const itemCounterContainer = document.querySelector('.itemCounterContainer');
-    
     itemCounter();
-    console.log(itemCounterContainer);
-    // let value = itemCounterContainer.textContent;
+    let value = itemCounterContainer.textContent;
     expect(value).toBe('Meals(6)');
   });
 });
