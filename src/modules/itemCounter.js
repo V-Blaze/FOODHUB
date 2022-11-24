d6e5a6961028873029725a31e2fa32949ea38f66
Usama Masood <usamamaliq@gmail.com>
2022-11-24 07:56:48 +0500

    Fix linter errors

--- src/modules/itemCounter.js
+++ src/modules/itemCounter.js
@@ -1,6 +1,6 @@
 const itemCounter = () => {
-const countAll = document.querySelectorAll('.mainItemContainer').length;
-const itemCounterContainer = document.querySelector('.itemCounterContainer');
-itemCounterContainer.innerHTML = `Meals(${countAll})`;
+  const countAll = document.querySelectorAll('.mainItemContainer').length;
+  const itemCounterContainer = document.querySelector('.itemCounterContainer');
+  itemCounterContainer.innerHTML = `Meals(${countAll})`;
 };
 export default itemCounter;
